01bda5597e4a885927a52db769ce027319b358b0
b-raymond <braymond@regis.edu>
2017-07-24 15:15:42 -0600

    initialcommit

---
+++ src/main/java/org/charter/raymond/quickstart/App.java
@@ -0,0 +1,17 @@
+package org.charter.raymond.quickstart;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    		Gson gson = new Gson();
+        System.out.println( "Hello World!" );
+    }
+}
